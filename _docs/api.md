911db6ce16aea3fca74e342eedfdab7a1032f658
s.yamazaki <s.yamazaki@gigei.jp>
2015-07-14 21:08:55 +0900

    リアルタイムプレビュー

---
+++ _docs/api.md
@@ -0,0 +1,5 @@
+FORMAT: 1A
+
+# Polls
+
+Polls is a simple API allowing consumers to view polls and vote in them.
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,35 @@
 var gulp = require('gulp'),
-	aglio = require('gulp-aglio');
+    aglio = require('gulp-aglio'),
+    browserSync = require('browser-sync');
+    rename = require('gulp-rename');
+
+var reload = browserSync.reload;
+
+var TEMPLATE_FILES = ['_docs/*.md'],
+    PUBLISHED_DIR = 'docs';
 
 gulp.task('docs', function () {
   gulp.src('_docs/*.md')
     .pipe(aglio({ template: 'default' }))
+    .pipe(rename('index.html'))
     .pipe(gulp.dest('docs'));
 });
+
+gulp.task('watch', function () {
+  gulp.watch(TEMPLATE_FILES, ['docs', reload]);
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    logConnections: true,
+    logFileChanges: true,
+    notify: true,
+    port: 8088,
+    open: false,
+    server: {
+      baseDir: PUBLISHED_DIR
+    }
+  });
+});
+
+gulp.task('default', ['docs', 'watch', 'browserSync']);
